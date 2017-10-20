224c9ef55364f676ac5cb3dd51bd0d163d1475e3
Li <zli14@wpi.edu>
2017-10-20 11:52:49 -0400

    add train file

---
+++ train.py
@@ -0,0 +1,16 @@
+import tensorflow as tf
+import datetime
+import os
+import argparse
+import glob
+
+from model.model import Net
+
+class Solver:
+    
+    def load_data(self, image_dir, label_dir):
+        image_path = dir+'/*.jpg'
+    
+    
+    def train(self):
+        
