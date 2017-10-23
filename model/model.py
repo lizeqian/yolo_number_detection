93289c7c9e5a0b0958cf02e9fb1db8c5a644afc3
Li <zli14@wpi.edu>
2017-10-23 09:54:27 -0400

    complete training model

--- model/model.py
+++ model/model.py
@@ -2,35 +2,44 @@ import tensorflow as tf
 import numpy as np
 
 class Net:
-    def __init__(self, is_training=True):
+    def __init__(self, is_training, batch_size):
 
         self.w_layer_1 = [7,7,1,32]
-        self.w_layer_2 = [3,3,self.layer_1[3],64]
-        self.w_layer_3 = [3,3,self.layer_2[3],128]
-        self.w_layer_4 = [3,3,self.layer_3[3],256]
-        self.w_layer_5 = [3,3,self.layer_4[3],512]
-        self.w_layer_6 = [3,3,self.layer_5[3],1024]
+        self.w_layer_2 = [3,3,self.w_layer_1[3],64]
+        self.w_layer_3 = [3,3,self.w_layer_2[3],128]
+        self.w_layer_4 = [3,3,self.w_layer_3[3],256]
+        self.w_layer_5 = [3,3,self.w_layer_4[3],512]
+        self.w_layer_6 = [3,3,self.w_layer_5[3],1024]
 
-        self.b_layer_1 = self.w_layer_1[3]
-        self.b_layer_2 = self.w_layer_2[3]
-        self.b_layer_3 = self.w_layer_3[3]
-        self.b_layer_4 = self.w_layer_4[3]
-        self.b_layer_5 = self.w_layer_5[3]
-        self.b_layer_6 = self.w_layer_6[3]
-        self.images = tf.placeholder(tf.float32, [None, 448, 448, 1], name='images')
-        self.logits = self.net_6_layers(self.images, is_training=is_training)
+        self.b_layer_1 = [self.w_layer_1[3]]
+        self.b_layer_2 = [self.w_layer_2[3]]
+        self.b_layer_3 = [self.w_layer_3[3]]
+        self.b_layer_4 = [self.w_layer_4[3]]
+        self.b_layer_5 = [self.w_layer_5[3]]
+        self.b_layer_6 = [self.w_layer_6[3]]
 
-        self.batch_size = 100
-        self.offset = np.transpose(np.reshape(np.array([np.arange(7)] * 7 * 2), (2, 7, 7)), (1, 2, 0))
+        self.batch_size = batch_size
 
         self.lambda_noobj = 0.5
-        self.lambda_coord = 5
+        self.lambda_coord = 2
         
-        if is_training:
-            self.labels = tf.placeholder(tf.float32, [None, 7, 7, 2*5+2])
-            self.loss_function(self.logits, self.labels)
-            self.total_loss = tf.losses.get_total_loss()
-            
+        self.image_size = 112
+        self.cell_size = 7
+        
+        self.offset = np.transpose(np.reshape(np.array(
+            [np.arange(7)] * 7 * 2),
+            (2, 7, 7)), (1, 2, 0))
+        
+    def variable_summaries(self, var, scope): #"""Attach a lot of summaries to a Tensor (for TensorBoard visualization)."""
+        with tf.name_scope(scope):
+            mean = tf.reduce_mean(var)
+            tf.summary.scalar('mean', mean)
+            with tf.name_scope('stddev'):
+                stddev = tf.sqrt(tf.reduce_mean(tf.square(var - mean)))
+            tf.summary.scalar('stddev', stddev)
+            tf.summary.scalar('max', tf.reduce_max(var))
+            tf.summary.scalar('min', tf.reduce_min(var))
+            tf.summary.histogram('histogram', var)        
 
     def conv2d(self, x, w, b):
         conv_ = tf.nn.conv2d(x, w, strides=[1,1,1,1], padding='SAME')
@@ -53,9 +62,9 @@ class Net:
 
     def loss_variable(self, shape):
         initial = tf.constant(0.0001, shape=shape)
-        return tf.Variable(initial) 
+        return tf.Variable(initial, trainable=False) 
 
-    def net_6_layers(self,image,is_training=True):
+    def net_4_layers(self,image,is_training):
         l1_w = self.weight_variable(self.w_layer_1)
         l1_b = self.bias_variable(self.b_layer_1)
         l2_w = self.weight_variable(self.w_layer_2)
@@ -72,19 +81,29 @@ class Net:
         layer2_out = self.conv2d(layer1_out, l2_w, l2_b)
         layer3_out = self.conv2d(layer2_out, l3_w, l3_b)
         layer4_out = self.conv2d(layer3_out, l4_w, l4_b)
-        layer5_out = self.conv2d(layer4_out, l5_w, l5_b)
-        layer6_out = self.conv2d(layer5_out, l6_w, l6_b)
+#        layer5_out = self.conv2d(layer4_out, l5_w, l5_b)
+#        layer6_out = self.conv2d(layer5_out, l6_w, l6_b)
         
-        layer6_out_flat = tf.reshape(layer6_out, [-1, 7*7*self.w_layer_6[3]])
-        fc1_w = self.weight_variable([7*7*self.w_layer_6[3], 4096])
+#        layer6_out_flat = tf.reshape(layer6_out, [-1, 7*7*self.w_layer_6[3]])
+        layer4_out_flat = tf.reshape(layer4_out, [-1, 7*7*self.w_layer_4[3]])
+        fc1_w = self.weight_variable([7*7*self.w_layer_4[3], 4096])
         fc1_b = self.bias_variable([4096])
         fc2_w = self.weight_variable([4096, 7*7*12])
         fc2_b = self.bias_variable([7*7*12])
-        fc1_out = self.fc(layer6_out_flat, fc1_w, fc1_b)
+        fc1_out = self.fc(layer4_out_flat, fc1_w, fc1_b)
         fc2_out = self.fc(fc1_out, fc2_w, fc2_b)   #[7*7*num_classes, 7*7*num_boxes*5]
         #fc2_out = tf.reshape(fc2_out, [-1, 7, 7, 7])
         
         return fc2_out
+    
+    def non_max_suppression(self, window, window_size):
+        # input: B x W x H x C
+        pooled = tf.nn.max_pool(window, ksize=[1, window_size, window_size, 1], strides=[1,1,1,1], padding='SAME')
+        output = tf.where(tf.equal(window, pooled), tf.ones_like(window), tf.zeros_like(window))
+    
+        # NOTE: if input has negative values, the suppressed values can be higher than original
+        return output # output: B X W X H x C
+
     
     def iou_calc(self, boxes1, boxes2): #x, y, w, h
         boxes1 = tf.stack([boxes1[:, :, :, :, 0] - boxes1[:, :, :, :, 2] / 2.0,
@@ -100,7 +119,7 @@ class Net:
         boxes2 = tf.transpose(boxes2, [1, 2, 3, 4, 0])
         
         # calculate the left up point & right down point
-        lu = tf.maximum(boxes1[:, :, :, :, :2], boxes2[:, :, :, :, :2])
+        lu = tf.maximum(boxes1[:, :, :, :, :2],- boxes2[:, :, :, :, :2])
         rd = tf.minimum(boxes1[:, :, :, :, 2:], boxes2[:, :, :, :, 2:])
         
         # intersection
@@ -128,9 +147,75 @@ class Net:
         iou = inter_area/(boxes0[2]*boxes0[3]+boxes1[2]+boxes1[3]-inter_area)
         
         return iou
-
+        
     
-    
+    def loss_function_vec(self, predicts, labels): #labels: [batch_size, cell_size_x, cell_size_y, 2+5] (x, y, w, h, C, p(c0), p(c1)) 
+        predict_class = tf.reshape(predicts[:,:7*7*2], [self.batch_size, 7, 7, 2]) #batch_size, cell_size, cell_size, num of class (class score)
+        predict_confidence = tf.reshape(predicts[:, 7*7*2:(7*7*2+7*7*2)], [self.batch_size, 7, 7, 2]) #batch_size, cell_size, cell_size, num of boxes (box confidence)
+        predict_boxes = tf.reshape(predicts[:, (7*7*2+7*7*2):], [self.batch_size, 7, 7, 2, 4]) # batch_size, cell_size, cell_size, boxes_num, 4 (box coordinate)
+        
+        gt_object = tf.reshape(labels[:, :, :, 4], [self.batch_size, self.cell_size, self.cell_size, 1])
+        gt_boxes = tf.reshape(labels[:, :, :, 0:4], [self.batch_size, self.cell_size, self.cell_size, 1, 4])
+        gt_boxes = tf.tile(gt_boxes, [1, 1, 1, 2, 1])
+        gt_classes = labels[:, :, :, 5:]
+        
+        offset = tf.constant(self.offset, dtype=tf.float32)
+        offset = tf.reshape(offset, [1, 7, 7, 2])
+        offset = tf.tile(offset, [self.batch_size, 1, 1, 1])
+        predict_boxes_tran = tf.stack([(predict_boxes[:, :, :, :, 0] + offset) * 16,
+                                           (predict_boxes[:, :, :, :, 1] + tf.transpose(offset, (0, 2, 1, 3))) * 16,
+                                           tf.square(predict_boxes[:, :, :, :, 2]) * 112,
+                                           tf.square(predict_boxes[:, :, :, :, 3]) * 112])
+        predict_boxes_tran = tf.transpose(predict_boxes_tran, [1, 2, 3, 4, 0])
+        
+        gt_boxes_tran = tf.stack([(gt_boxes[:, :, :, :, 0] + offset) * 16,
+                                           (gt_boxes[:, :, :, :, 1] + tf.transpose(offset, (0, 2, 1, 3))) * 16,
+                                           tf.square(gt_boxes[:, :, :, :, 2]) * 112,
+                                           tf.square(gt_boxes[:, :, :, :, 3]) * 112])
+        gt_boxes_tran = tf.transpose(gt_boxes_tran, [1, 2, 3, 4, 0])
+        
+        gt_iou = self.iou_calc(predict_boxes_tran, gt_boxes_tran)
+        max_iou = tf.reduce_max(gt_iou, axis = 3, keep_dims = True)
+        object_mask = tf.cast((gt_iou >= max_iou), tf.float32) * gt_object
+        noob_mask = tf.ones_like(object_mask, dtype=tf.float32) - object_mask
+        
+        #class loss
+        delta_p = gt_classes - predict_class
+        delta_p_obj = delta_p * gt_object
+        class_loss = tf.reduce_mean(tf.reduce_sum(tf.square(delta_p_obj), axis=[1,2,3]))
+        
+        #coord loss
+        coord_mask = tf.expand_dims(object_mask, 4)
+        coord_delta = predict_boxes[:,:,:,:,:2] - gt_boxes[:,:,:,:,:2]
+        coord_delta_mask = coord_delta * coord_mask
+        size_delta = tf.sqrt(predict_boxes[:,:,:,:,2:]) - tf.sqrt(gt_boxes[:,:,:,:,2:])
+        size_delta_mask = size_delta * coord_mask
+        coord_loss = (tf.reduce_mean(tf.reduce_sum(tf.square(coord_delta_mask), axis=[1,2,3,4])) + \
+                      tf.reduce_mean(tf.reduce_sum(tf.square(size_delta_mask), axis=[1,2,3,4])))*self.lambda_coord
+                                                
+        #iou loss
+        confidence_delta = predict_confidence - gt_iou
+        
+        iou_loss = (tf.reduce_mean(tf.reduce_sum(tf.square(confidence_delta*object_mask), axis=[1,2,3])))+   \
+                        self.lambda_noobj * (tf.reduce_mean(tf.reduce_sum(tf.square(confidence_delta*noob_mask), axis=[1,2,3])))
+        
+        #Accuracy IOU
+        pr_iou = tf.reduce_max(gt_iou, axis = 3, keep_dims = True)
+        pr_class = tf.reduce_max(predict_class, axis = 3, keep_dims = True)
+        pr_c = pr_iou * pr_class
+        pr_c_nms = self.non_max_suppression(pr_c, 3)
+        sel_iou = pr_iou*pr_c_nms
+        accu = tf.reduce_mean(tf.reduce_sum(sel_iou, axis=[1,2,3]))
+                
+        tf.losses.add_loss(class_loss)
+        tf.losses.add_loss(coord_loss)
+        tf.losses.add_loss(iou_loss)
+        
+        self.variable_summaries(class_loss, 'class_loss')
+        self.variable_summaries(coord_loss, 'coord_loss')
+        self.variable_summaries(iou_loss, 'iou_loss')
+        
+        return tf.losses.get_total_loss(), accu
 
     def loss_function(self, predicts, labels): #labels: [batch_size, cell_size_x, cell_size_y, 2+5] (x, y, w, h, C, p(c0), p(c1)) 
         predict_class = tf.reshape(predicts[:,:7*7*2], [self.batch_size, 7, 7, 2]) #batch_size, cell_size, cell_size, num of class (class score)
@@ -138,9 +223,9 @@ class Net:
         predict_boxes = tf.reshape(predicts[:, (7*7*2+7*7*2):], [self.batch_size, 7, 7, 2, 4]) # batch_size, cell_size, cell_size, boxes_num, 4 (box coordinate)
         
         #calculate loss
-        class_loss = self.loss_variable([self.batch_size])
-        iou_loss = self.loss_variable([self.batch_size])
-        coord_loss = self.loss_variable([self.batch_size])
+        class_loss = tf.convert_to_tensor(np.zeros(self.batch_size), dtype=tf.float32)#self.loss_variable([self.batch_size])
+        iou_loss = tf.convert_to_tensor(np.zeros(self.batch_size), dtype=tf.float32)#self.loss_variable([self.batch_size])
+        coord_loss = tf.convert_to_tensor(np.zeros(self.batch_size), dtype=tf.float32)#self.loss_variable([self.batch_size])
         
 #       for i in range(self.batch_size):
 #           for x in range(7):
@@ -156,21 +241,21 @@ class Net:
         for i in range(self.batch_size):
             for x in range(7):
                 for y in range(7):
-                    if labels[i, x, y, ] == 1:
-                        class_loss[i] += tf.square(labels[i, x, y, 5]-predict_class[i, x, y, 0]) + tf.square(labels[i, x, y, 6]-predict_class[i, x, y, 1])
+                    if labels[i, x, y, 4] == 1:
+                        class_loss_a = tf.assign(class_loss[i], tf.square(labels[i, x, y, 5]-predict_class[i, x, y, 0]) + tf.square(labels[i, x, y, 6]-predict_class[i, x, y, 1]) + class_loss[i])
                     for b in range(2):
                         boxes0 = predict_boxes[i, x, y, b]
                         boxes1 = label_boxes[i, x, y]
                         true_iou = self.iou_calc_loop(boxes0, boxes1)
                         pred_confidence = predict_scales[i, x, y, b]
                         if labels[i, x, y, 4] == 1 and pred_confidence == tf.reduce_max(predict_scales[i,x, y, :]):
-                            iou_loss[i] += tf.square(pred_confidence - true_iou)
-                            coord_loss[i] += self.lambda_coord*(tf.square(predict_boxes[i, x, y, b, 0]-label_boxes[i, x, y, 0])+  \
+                            iou_loss_a = tf.assign(iou_loss[i], tf.square(pred_confidence - true_iou) + iou_loss[i])
+                            coord_loss_a = tf.assign(coord_loss[i], self.lambda_coord*(tf.square(predict_boxes[i, x, y, b, 0]-label_boxes[i, x, y, 0])+  \
                                                                 tf.square(predict_boxes[i, x, y, b, 1]-label_boxes[i, x, y, 1])+  \
                                                                 tf.square(tf.sqrt(predict_boxes[i, x, y, b, 2]-label_boxes[i, x, y, 2]))+  \
-                                                                tf.square(tf.sqrt(predict_boxes[i, x, y, b, 3]-label_boxes[i, x, y, 3])))
+                                                                tf.square(tf.sqrt(predict_boxes[i, x, y, b, 3]-label_boxes[i, x, y, 3]))) + coord_loss[i])
                         else:
-                            iou_loss[i] += self.lambda_noobj*tf.square(pred_confidence)
+                            iou_loss_a = tf.assign(iou_loss[i], self.lambda_noobj*tf.square(pred_confidence)+iou_loss[i])
 
         #calculate coordinate loss 
 
@@ -180,8 +265,8 @@ class Net:
 #                   for b in range(2):
 #                       pred_confidence = predict_scales[i, x, y, b]
 #                       if labels[i, x, y, 4] == 1 and pred_confidence == tf.reduce_max(predict_scales[i,x, y, :]):
-
-        tf.losses.add_loss(class_loss)
-        tf.losses.add_loss(iou_loss)
-        tf.losses.add_loss(coord_loss)
+        return class_loss + iou_loss + coord_loss
+#        tf.losses.add_loss(class_loss)
+#        tf.losses.add_loss(iou_loss)
+#        tf.losses.add_loss(coord_loss)
 
--- train.py
+++ train.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+import numpy as np
 import datetime
 import os
 import argparse
@@ -6,11 +7,85 @@ import glob
 
 from model.model import Net
 
+def read_image(filename_queue):
+    label = filename_queue[1]
+    file_contents = tf.read_file(filename_queue[0])
+    image = tf.image.decode_jpeg(file_contents)
+    return image, label
+
 class Solver:
+    def __init__(self, num_epochs, batch_size,  net):
+        self.num_epochs = num_epochs
+        self.net = net
+        self.batch_size = batch_size
     
     def load_data(self, image_dir, label_dir):
-        image_path = dir+'/*.jpg'
-    
-    
+        image_path = image_dir+'/*.png'
+        image_addrs = glob.glob(image_path)  
+        image_labels = np.genfromtxt(label_dir, delimiter=',')
+        image_labels = np.reshape(image_labels, [-1, 7, 7, 7])
+        filename_queue = tf.train.slice_input_producer([image_addrs, image_labels], num_epochs=self.num_epochs, shuffle=False)
+        image, label = read_image(filename_queue)
+        image.set_shape((112, 112, 1))
+        image_batch, label_batch = tf.train.batch([image, label], batch_size=self.batch_size)
+        return image_batch, label_batch
+        
     def train(self):
+        sess = tf.InteractiveSession()
+        
+        image_batch, label_batch = self.load_data('detection_training', 'detection_training.csv')
+        print ("Data loaded")
+        image = tf.placeholder(tf.float32, shape=[None, 112,112,1])
+        label = tf.placeholder(tf.float32, shape=[None, 7, 7, 7])
+        net_out = self.net.net_4_layers(image, True)
+        total_loss, accu = self.net.loss_function_vec(net_out, label)
+        tf.summary.scalar('total_loss', total_loss)
+        tf.summary.scalar('accu', accu)
+        merged = tf.summary.merge_all()
+        writer = tf.summary.FileWriter('./log/', sess.graph)
+        print ("Network built")
+        update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+
+        with tf.control_dependencies(update_ops):
+            train_step = tf.train.AdamOptimizer(1e-4).minimize(total_loss)   
+        init_op = tf.group(tf.global_variables_initializer(), tf.local_variables_initializer())
+        
+        saver = tf.train.Saver()
+        
+
+        sess.run(init_op)
+        if False:
+            saver.restore(sess, "./tmp/My_Model.ckpt")
+            print("Model restored.")
+        coord = tf.train.Coordinator()
+        threads = tf.train.start_queue_runners(coord=coord)
+
+        print ("Begin Training")
+        for epoch in range(0, self.num_epochs):
+            image_tensor,image_label = sess.run([image_batch,label_batch])
+            summary, _ = sess.run([merged, train_step], feed_dict={image: image_tensor, label: image_label})
+            if epoch%100 == 0:
+                accuracy = accu.eval(feed_dict={image: image_tensor, label: image_label})
+                print (accuracy)
+            writer.add_summary(summary, epoch)
+        writer.close()    
+        coord.request_stop()
+        coord.join(threads)
+        
+        save_path = saver.save(sess, "./tmp/My_Model.ckpt")
+        print("Model saved in file: %s" % save_path)
+
+
+
+            
+def main():
+    ne = 10000
+    bs = 50
+    net = Net(True, bs)
+    solver = Solver(ne, bs, net)
+    
+    solver.train()
+    
+if __name__ == '__main__':
+    main()
         
