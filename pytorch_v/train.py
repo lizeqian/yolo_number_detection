87e56737829568725cecdc9db7df498d3db45706
Li <zli14@wpi.edu>
2017-10-26 18:56:05 -0400

    add

--- pytorch_v/train.py
+++ pytorch_v/train.py
@@ -7,7 +7,7 @@ from torch.utils.data.dataset import Dataset
 from torch.utils.data import DataLoader
 from torch.utils.data.sampler import Sampler
 
-class solver:
+class Solver:
     
     def image_read(self, dir):
         normalize = transforms.Normalize(
@@ -24,3 +24,14 @@ class solver:
         img_pil = Image.open(dir)
         img_tensor = preprocess(img_pil)
         img_tensor.unsqueeze_(0)
+        
+class Rand_num(Dataset):
+    def __init__(self, csv_path, img_path):
+        self.img_path = img_path
+        self.csv_path = csv_path
+        
+    def __getitem__(self, index):
+        pass
+        
+    def __len__(self):
+        pass
